4af7f4f6455ce805e71af79c25f3398c3c9d371d
Radu <rdmc10@github.com>
2026-02-22 02:22:51 +0200

    22.02

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Comic Mono:pixelsize=18:antialias=true:autohint=true";
 static char *font2[] = { "Font Awesome 7 Free:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 2;
 
@@ -95,38 +95,69 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
+//static const char *colorname[] = {
+//	"#1d2021", /* hard contrast: #1d2021 / soft contrast: #32302f */
+//	"#cc241d",
+//	"#98971a",
+//	"#d79921",
+//	"#458588",
+//	"#b16286",
+//	"#689d6a",
+//	"#a89984",
+//	"#928374",
+//	"#fb4934",
+//	"#b8bb26",
+//	"#fabd2f",
+//	"#83a598",
+//	"#d3869b",
+//	"#8ec07c",
+//	"#ebdbb2",
+//	[255] = 0,
+//	/* more colors can be added after 255 to use with DefaultXX */
+//	"#add8e6", /* 256 -> cursor */
+//	"#555555", /* 257 -> rev cursor*/
+//	"#282828", /* 258 -> bg */
+//	"#ebdbb2", /* 259 -> fg */
+//};
+
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
+        /* 8 normal colors */
+        "black",
+        "red3",
+        "green3",
+        "yellow3",
+        "blue2",
+        "magenta3",
+        "cyan3",
+        "gray90",
+
+        /* 8 bright colors */
+        "gray50",
+        "red",
+        "green",
+        "yellow",
+        "#5c5cff",
+        "magenta",
+        "cyan",
+        "white",
+
+        [255] = 0,
+
+        /* more colors can be added after 255 to use with DefaultXX */
+        "#cccccc",
+        "#555555",
+        "gray90", /* default foreground colour */
+        "black", /* default background colour */
 };
+
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
